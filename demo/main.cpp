e5e4d1fcd7d9c8e89e45607fa4b0ed307e30e2a6
ProkiProk <senior.prockuschev2017@yandex.ru>
2022-02-16 00:21:05 +0300

    все кроме деструктора и тестов

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,7 @@
 #include <example.hpp>
+
 #include "iostream"
+#include "secex.h"
 // стивен прат 10 глава
 // копирование - реализуем оператор копирования, в конце надо писать delete
 // 1. конструктор по умолчанию
@@ -20,18 +22,91 @@
 // Оформлять как Stack1  Stack2
 // 2 хедера hpp
 //
+
+class Item {
+ private:
+  int f;
+  double ft;
+
+ public:
+  Item() {
+    f = -1;
+    ft = -1;
+  }
+  Item(int fi) {
+    this->f = fi;
+    this->ft = 0;
+  }
+  Item(int fi, double fth) {
+    this->f = fi;
+    this->ft = fth;
+  }
+  bool operator!() const {
+    if (this->f != -1) {
+      return false;
+    }
+    if (this->ft != -1) {
+      return false;
+    }
+    return true;
+  };
+  Item& operator=(Item* right) noexcept {
+    f = right->f;
+    ft = right->ft;
+    return *this;
+  }
+  Item& operator=(const Item& right) = default;
+  //  Item operator*(Item* item){
+  //    this->f = item->f;
+  //    this->ft = item->ft;
+  //  }
+  void print() const {
+    std::cout << f << std::endl;
+    std::cout << ft << std::endl;
+  }
+};
+
 int main() {
+  //  TODO 1 ex
   Stack<int> stack;
+  const int& left = 50;
   stack.push(10);
+  std::cout << stack.head() << std::endl;
+
   stack.push(20);
+  std::cout << stack.head() << std::endl;
   stack.push(30);
+  std::cout << stack.head() << std::endl;
   stack.push(40);
-  std::cout << stack.head()<< std::endl;
+  std::cout << stack.head() << std::endl;
+  stack.push(left);
+  std::cout << stack.head() << std::endl;
   stack.pop();
-  std::cout << stack.head()<< std::endl;
+  std::cout << stack.head() << std::endl;
   stack.pop();
-  std::cout << stack.head()<< std::endl;
- stack.pop();
-  std::cout << stack.head()<< std::endl;
-//  stack.
+  std::cout << stack.head() << std::endl;
+  stack.pop();
+  std::cout << stack.head() << std::endl;
+  std::cout << "Stack " << stack.head() << std::endl;
+  Stack<int> stack1;
+  stack1.push(333);
+  //   Оператор перемещения.
+  stack1 = Stack<int>(444);
+  std::cout << "Stack " << stack.head() << std::endl;
+  std::cout << "Stack1 " << stack1.head() << std::endl;
+  //  TODO 1 ex END
+  //  TODO 2 ex
+  //  StackSec<Item> stackSec(Item{11});
+  //  stackSec.head().print();
+  //  stackSec.push_emplace(22, 22.0);
+  //  stackSec.head().print();
+  //  stackSec.push_emplace(33, 33.0);
+  //  stackSec.head().print();
+  //  stackSec.push_emplace(44, 44.0);
+  //  stackSec.head().print();
+  //  Item val = stackSec.pop();
+  //  val.print();
+  //  stackSec.head().print();
+  //  stackSec.push_emplace(55);
+  //  stackSec.head().print();
 }
--- include/secex.h
+++ include/secex.h
@@ -1,32 +1,50 @@
-// Copyright 2021 Your Name <your_email>
+//
+// Created by prokiprok on 14.02.2022.
+//
 
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
+#ifndef TEMPLATE_SECEX_H
+#define TEMPLATE_SECEX_H
 #include <stdexcept>
 
-template <typename T>
-struct Element {
-  T value;
-  Element* link;
-};
+#include "example.hpp"
 
 template <typename T>
-class Stack {
+class StackSec {
  private:
   Element<T>* last;
   Element<T>* start;
 
  public:
-  Stack() {
+  StackSec() {
     this->last = new Element<T>;
     this->start = new Element<T>;
-  };
-  explicit Stack(T valueStart) {
+  }
+  explicit StackSec(T valueStart) {
     this->last = new Element<T>;
     this->start = new Element<T>;
     last->value = valueStart;
     last->link = NULL;
     this->start = this->last;
+  }
+  ~StackSec() {
+    //  TODO
+  }
+  template <typename... Args>
+  void push_emplace(Args&&... args) {
+    if (!this->start->value) {
+      last->value = std::move(T(std::forward<Args>(args)...));
+      last->link = NULL;
+      this->start = this->last;
+      return;
+    }
+    Element<T>* temp;
+    Element<T>* oldlink;
+    temp = new Element<T>;
+    oldlink = last->link;
+    last->link = temp;
+    temp->value = std::move(T(std::forward<Args>(args)...));
+    temp->link = oldlink;
+    this->last = temp;
   }
   void push(T&& value) {
     if (!this->start->value) {
@@ -37,62 +55,25 @@ class Stack {
     }
     Element<T>* temp;
     Element<T>* oldlink;
-    //    temp = (Element<T>*)malloc(sizeof(Element<T>));
     temp = new Element<T>;
     oldlink = last->link;
-    last->link = temp;
     temp->value = std::move(value);
     temp->link = oldlink;
     this->last = temp;
   };
-
-  void push(const T& value) {
-    if (!this->start->value) {
-      last->value = value;
-      last->link = NULL;
-      this->start = this->last;
-      return;
-    }
-    Element<T>* temp;
-    Element<T>* oldlink;
-    temp = new Element<T>;
-    oldlink = last->link;
-    last->link = temp;
-    temp->value = value;
-    temp->link = oldlink;
-    this->last = temp;
-  };
-  void pop() {
+  const T& head() const { return this->last->value; };
+  T pop() {
     Element<T>* temp;
     temp = this->start;
     while (temp->link != this->last) {
       temp = temp->link;
     }
     temp->link = last->link;
+    T value = last->value;
     delete (last);
     this->last = temp;
+    return value;
   };
-  const T& head() const { return this->last->value; };
-  Stack<T>& operator=(const Stack<T>& right) = delete;
-  Stack<T>& operator=(Stack<T>&& right) {
-    if (this->last == this->start) {
-      delete (this->last);
-    } else {
-      delete (this->last);
-      delete (this->start);
-    }
+ };
 
-    this->last = std::move(right.last);
-    this->start = std::move(right.start);
-    right.last = nullptr;
-    right.start = nullptr;
-    return *this;
-  }
-  ~Stack() {
-//    TODO
-  }
-};
-
-auto example() -> void;
-
-#endif  // INCLUDE_EXAMPLE_HPP_
+#endif  // TEMPLATE_SECEX_H
